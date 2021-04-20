96eb19e409918bae53267f76b1e214c07951d536
Alfred Arinze <fredintek@gmail.com>
2021-04-20 09:40:56 +0300

    Added a divide function and printed system version

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
+import sys
+
+print(sys.version)
+
+
 def mul(x, y):
-    return x*y
+    return x * y
+
 
 def add(x, y):
-    return x+y
+    return x + y
+
+
+def divide(x, y):
+    return x / y
