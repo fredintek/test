bb0232c84cd4cb98769343800a36f7321bb29f71
Alfred Arinze <fredintek@gmail.com>
2021-04-20 11:43:42 +0300

    Simple example on how decorators work with functions

---
+++ test.py
@@ -0,0 +1,27 @@
+import sys
+
+print(sys.version)
+
+
+def add_2(original_function):
+    def wrapper_function(*args, **kwargs):
+        print(f'we added 2 to the result of {original_function.__name__} function')
+        print(f'The result of the original function is {original_function(*args, **kwargs)}')
+        return original_function(*args, **kwargs) + 2
+    return wrapper_function
+
+
+@add_2
+def divide(x, y):
+    return x / y
+
+
+print(divide(4, 2))
+
+
+@add_2
+def mul(x, y):
+    return x * y
+
+
+print(mul(4, 2))
